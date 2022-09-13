74c764dc391166dba184151e0be8d3be771d2ba3
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-09-13 13:25:58 +0800

    migrate from css to scss, rename prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inst-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-first-project';
